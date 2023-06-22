e53628a2212b2225ef8cced93ea28290abb7aa5c
Pastafredda <justspotify09@gmail.com>
2023-06-22 18:42:40 +0200

    fine + bonus

--- php/index.php
+++ php/index.php
@@ -5,23 +5,5 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 header('Content-Type: application/json');
 
-    $items = [
-        [
-            'text'=> "HTML",
-            'done'=>  true
-        ],
-        [
-            'text'=> "CSS",
-            'done'=>  false
-        ],
-        [
-            'text'=> "Responsive Design",
-            'done'=>  true
-        ],
-        [
-            'text'=> "Javascript",
-            'done'=>  false
-        ],
-];
-
-echo json_encode($items);
+$itemsStr = file_get_contents('data.json');
+echo $itemsStr;
--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,22 @@ export default {
       items: []
     }
   },
+  methods: {
+    rimuovi(indice) {
+      this.items.splice(indice, 1)
+    },
+    aggiungi() {
+      this.items.unshift({ text: this.items.text })
+      this.items.text = "";
+    },
+    changeDone(indice) {
+      if (!this.items[indice].done) {
+        this.items[indice].done = true;
+      } else {
+        this.items[indice].done = false;
+      }
+    }
+  },
   mounted() {
     console.log("hello world");
 
@@ -19,7 +35,95 @@ export default {
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <div class="container">
+    <header>
+      <h1>To do List</h1>
+    </header>
+    <div class="aggiungi-task">
+      <input type="text" placeholder="Inserisci Task" v-model="items.text" @keyup.enter="aggiungi" />
+      <button @click="aggiungi">Aggiungi</button>
+    </div>
+
+    <div class="task-container">
+      <ul>
+        <li @click.stop="changeDone(index)" class="task" :class="task.done ? 'sbarrato' : ''"
+          v-for="(task, index,) in items">
+          {{ task.text }}<i class="fa-solid fa-x" @click.stop="rimuovi(index)"></i>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: 0;
+}
+
+.container {
+  margin: auto;
+}
+
+header {
+  text-align: center;
+  margin-top: 100px;
+  font-size: 24px;
+  color: rgb(203, 0, 0);
+}
+
+.aggiungi-task {
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+}
+
+.aggiungi-task input {
+  border-radius: 8px;
+  padding: 0 8px;
+  width: 400px;
+  height: 30px;
+}
+
+.aggiungi-task button {
+  margin-left: 10px;
+  width: 100px;
+  border-radius: 8px;
+}
+
+.task-container {
+  display: flex;
+  justify-content: center;
+
+
+}
+
+.task-container ul {
+  width: 600px;
+  margin: 40px;
+  background-color: #e9e9ed;
+  border-radius: 12px;
+}
+
+.task {
+  width: 80%;
+  margin: auto;
+  padding: 5px 20px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+  list-style-type: none;
+  border-bottom: 1px solid rgb(200, 200, 200);
+  cursor: pointer;
+}
+
+
+.task .fa-x {
+  float: right;
+  cursor: pointer;
+}
+
+.sbarrato {
+  text-decoration: line-through;
+}
+</style>
